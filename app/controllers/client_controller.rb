35afeee809d7d54e1d5d10db249c85dec34dd6b4
Gustave L Rosanio <gusrosanio@yahoo.com>
2018-11-07 17:24:00 +0000

    changed controller file and added a client model

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,3 +1,15 @@
 class ClientController < ApplicationController
+    require 'twitter'
+    def new
+        @client = Twitter::REST::Client.new do |config|
+            config.consumer_key = "dJaGLikNUVQN8f8JMKs9rqY9k"
+            config.consumer_secret = "sGNrb28QTMe41K1lfDgYYeUKFVtJ6MCpj4mCWo4m1Zcc7vc7LD"
+            config.access_token = "866054735882842112-eV3q0dG3AHGqxaQHbXAZDfndVf8qwkB"
+            config.access_token_secret = "rAS2pKxnv9gyN98X2JgOw74mwEaURhIEx6Diz4cZqc6wX"
+      end
+    end
     
+    def search
+        
+    end
 end
