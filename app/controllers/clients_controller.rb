6134952e35c32b605691e6d68077bb4fc70f4fe4
Gustave L Rosanio <gusrosanio@yahoo.com>
2018-11-07 23:50:29 +0000

    added an index page and configured controller to process things properly

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -11,6 +11,18 @@ class ClientsController < ApplicationController
       end
     end
     
+    def index
+        require 'rubygems'
+        require 'bundler/setup'
+        require 'twitter'
+        @client = Twitter::REST::Client.new do |config|
+            config.consumer_key = "dJaGLikNUVQN8f8JMKs9rqY9k"
+            config.consumer_secret = "sGNrb28QTMe41K1lfDgYYeUKFVtJ6MCpj4mCWo4m1Zcc7vc7LD"
+            config.access_token = "866054735882842112-eV3q0dG3AHGqxaQHbXAZDfndVf8qwkB"
+            config.access_token_secret = "rAS2pKxnv9gyN98X2JgOw74mwEaURhIEx6Diz4cZqc6wX"
+      end
+    end
+    
     def create
         @client = Twitter::REST::Client.new do |config|
             config.consumer_key = "dJaGLikNUVQN8f8JMKs9rqY9k"
@@ -18,11 +30,11 @@ class ClientsController < ApplicationController
             config.access_token = "866054735882842112-eV3q0dG3AHGqxaQHbXAZDfndVf8qwkB"
             config.access_token_secret = "rAS2pKxnv9gyN98X2JgOw74mwEaURhIEx6Diz4cZqc6wX"
       end
-      redirect_to client_show(@client)
+      redirect_to clients_path(@client)
     end
     
     private
-    def coupon_params
-        params.require(:coupon).permit(:store, :coupon_code)
+    def client_params
+        params.require().permit()
     end
 end
