0f2b3c783aa97e4f0d5afae959813033c3d8d9b4
Gustave L Rosanio <gusrosanio@yahoo.com>
2018-11-07 22:34:08 +0000

    added form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -9,6 +9,20 @@ class ClientsController < ApplicationController
             config.access_token = "866054735882842112-eV3q0dG3AHGqxaQHbXAZDfndVf8qwkB"
             config.access_token_secret = "rAS2pKxnv9gyN98X2JgOw74mwEaURhIEx6Diz4cZqc6wX"
       end
-      #@client = Twitter::REST::Client.new
+    end
+    
+    def create
+        @client = Twitter::REST::Client.new do |config|
+            config.consumer_key = "dJaGLikNUVQN8f8JMKs9rqY9k"
+            config.consumer_secret = "sGNrb28QTMe41K1lfDgYYeUKFVtJ6MCpj4mCWo4m1Zcc7vc7LD"
+            config.access_token = "866054735882842112-eV3q0dG3AHGqxaQHbXAZDfndVf8qwkB"
+            config.access_token_secret = "rAS2pKxnv9gyN98X2JgOw74mwEaURhIEx6Diz4cZqc6wX"
+      end
+      redirect_to client_show(@client)
+    end
+    
+    private
+    def coupon_params
+        params.require(:coupon).permit(:store, :coupon_code)
     end
 end
