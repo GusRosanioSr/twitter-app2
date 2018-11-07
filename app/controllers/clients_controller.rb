e60349ee50c1f5c28247e26b19c38e0321dea3fc
Gustave L Rosanio <gusrosanio@yahoo.com>
2018-11-07 18:27:59 +0000

    worked on twitter gem and home.html

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,15 +1,14 @@
-class ClientController < ApplicationController
-    require 'twitter'
-    def new
+class ClientsController < ApplicationController
+    def home
+        require 'rubygems'
+        require 'bundler/setup'
+        require 'twitter'
         @client = Twitter::REST::Client.new do |config|
             config.consumer_key = "dJaGLikNUVQN8f8JMKs9rqY9k"
             config.consumer_secret = "sGNrb28QTMe41K1lfDgYYeUKFVtJ6MCpj4mCWo4m1Zcc7vc7LD"
             config.access_token = "866054735882842112-eV3q0dG3AHGqxaQHbXAZDfndVf8qwkB"
             config.access_token_secret = "rAS2pKxnv9gyN98X2JgOw74mwEaURhIEx6Diz4cZqc6wX"
       end
-    end
-    
-    def search
-        
+      @client = Twitter::Client.new
     end
 end
