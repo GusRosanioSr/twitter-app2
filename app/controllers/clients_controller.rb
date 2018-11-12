8520ac569956dd07220e31be11d0e9a5d57eddf6
Gustave L Rosanio <gusrosanio@yahoo.com>
2018-11-12 23:19:49 +0000

    changed stylesheet, worked on getting search result to appear

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -21,9 +21,14 @@ class ClientsController < ApplicationController
             config.access_token = "866054735882842112-eV3q0dG3AHGqxaQHbXAZDfndVf8qwkB"
             config.access_token_secret = "rAS2pKxnv9gyN98X2JgOw74mwEaURhIEx6Diz4cZqc6wX"
       end
+      @search = params[:search]
     end
     
     def create
+
+        require 'rubygems'
+        require 'bundler/setup'
+        require 'twitter'
         @client = Twitter::REST::Client.new do |config|
             config.consumer_key = "dJaGLikNUVQN8f8JMKs9rqY9k"
             config.consumer_secret = "sGNrb28QTMe41K1lfDgYYeUKFVtJ6MCpj4mCWo4m1Zcc7vc7LD"
@@ -35,6 +40,6 @@ class ClientsController < ApplicationController
     
     private
     def client_params
-        params.require().permit()
+        params.permit(:search)
     end
 end
